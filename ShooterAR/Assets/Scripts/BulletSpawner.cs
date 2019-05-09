5e129efc09517dc8e88268e997254ead7a326b9f
Bartlomiej Przybylak <bartlomiej.przybylak@gmail.com>
2019-05-09 17:27:21 +0200

    Animations for drones be born, EnemySpawner controls of number of drones, minor bugs

--- ShooterAR/Assets/Scripts/BulletSpawner.cs
+++ ShooterAR/Assets/Scripts/BulletSpawner.cs
@@ -9,31 +9,20 @@ namespace ShooterAR
         [SerializeField]
         private Transform gunForwardVector;
 
-        private bool unlockBullets = false;
-
         public void OnEnable()
         {
             Cannon.OnShoot += SpawnOneBullet;
-            GroundFinding.OnStartGame += UnlockBullets;
         }
 
         public void OnDisable()
         {
             Cannon.OnShoot -= SpawnOneBullet;
-            GroundFinding.OnStartGame -= UnlockBullets;
         }
 
         public void SpawnOneBullet(Vector3 spawnPoint)
         {
-            if (!unlockBullets)
-                return;
-            
             transform.GetChild(0).GetComponent<Bullet>().SpawnPoint = spawnPoint;
             transform.GetChild(0).GetComponent<Bullet>().Shoot(gunForwardVector.transform.forward);
-        }
-
-        private void UnlockBullets() {
-            unlockBullets = true;
         }
     }
 }
